e10cc4814fac8cb900274b408795ac6e7040dad3
annezzat <110825468+annezzat@users.noreply.github.com>
2022-08-08 21:47:11 +0200

    Update Monitor.sv

--- Monitor.sv
+++ Monitor.sv
@@ -9,12 +9,12 @@ class monitor;
     
     forever begin
       packet m_pkt = new();
-      @(posedge m_Clk_vif.tb_clk);
+      @(posedge m_clk_vif.tb_clk);
       #1;
       m_pkt.A = m_adder_vif.A;
       m_pkt.B = m_adder_vif.B;
       m_pkt.rstn = m_adder_vif.rstn;
-      m_pkt.ALU_sel = m_adder_vif.ALU_sel
+      m_pkt.ALU_sel = m_adder_vif.ALU_sel;
       m_pkt.ALU_out = m_adder_vif.ALU_out;
       m_pkt.Carry_out = m_adder_vif.Carry_out;
       m_pkt.print("Monitor");
