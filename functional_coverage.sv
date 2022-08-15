27a2ba64dd7394be814db8fb383fe48ac819445c
shaarawy29 <ashaarawy19@gmail.com>
2022-08-15 13:35:54 +0200

    test transition with multiple instance

--- functional_coverage.sv
+++ functional_coverage.sv
@@ -6,27 +6,36 @@ virtual ALU_if m_ALU_if;
 	real curr_func_cov;
 
 covergroup functional_cov;
-							//option.per_instance = 1;
+							option.per_instance = 1;
 		third_input_2: coverpoint m_ALU_if.ALU_sel { 
 									option.at_least = 4;
 									//option.per_instance = 1;
 									bins instructions[16] = {[0:15]} ;
                  }
-		transition: coverpoint m_ALU_if.ALU_sel {
+		/*transition: coverpoint m_ALU_if.ALU_sel {
 									option.at_least = 1;
 									//option.per_instance = 1;
 									bins t = (4'h0 => 4'h1 => 4'h2);
+		}*/
+endgroup
+covergroup functional_cov2;
+								option.at_least = 1;
+								option.per_instance = 1;
+transition: coverpoint m_ALU_if.ALU_sel {
+									bins t[1] = (4'h0 => 4'h1 => 4'h2);
+									//bins t = {4'h0};
 		}
 endgroup
-
  function new();
 			functional_cov = new;
+			functional_cov2 = new;
  endfunction
 
 task run();
 forever begin
 @(ok)
 functional_cov.sample();
+functional_cov2.sample();
 curr_func_cov = functional_cov.get_inst_coverage();
 $display("this coverage related to scoreboard = ", curr_func_cov );
 end
